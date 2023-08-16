275b78fe6b9ea5ef1173864c6c9a268bedf8f02c
LeiaWebDev <leia@cowmeuh.net>
2023-08-17 00:39:19 +0200

    added error handlers

--- server/routes/students.routes.js
+++ server/routes/students.routes.js
@@ -9,35 +9,40 @@ const Student = require("../models/students.model") // I need to access my colle
 
 // route to get all the students
 router.get('/students', async (req, res, next)=>{
+    console.log("Received a request for /students");
     try {
         const allStudents = await Student.find()
         res.json(allStudents)
 
     } catch (error) {
         console.log(error)
+        res.status(500).json({ message: "Internal server error" });
+        
     }
 })
 
 // route to get all students of a specific cohort
 router.get('/students/cohort/:cohortId', async (req, res, next)=>{
     try {
-        const studentsOfOneCohort = await Cohort.findById(req.params.id)
+        const studentsOfOneCohort = await Cohort.findById(req.params.cohortId)
         .populate("students")
-        res.json(studentsOfOneCohort)
+        res.json(studentsOfOneCohort.students)
 
     } catch (error) {
         console.log(error)
+        res.status(500).json({ message: "Internal server error" });
     }
 })
 
 // route to get one student by Id
 router.get('/students/:studentId', async (req, res, next)=>{
     try {
-        const oneStudent = await Student.findById(req.params,id)
+        const oneStudent = await Student.findById(req.params, studentId)
         res.json(oneStudent)
 
     } catch (error) {
         console.log(error)
+        res.status(500).json({ message: "Internal server error" });
     }
 })
 
@@ -46,41 +51,44 @@ router.post('/students', async(req, res, next)=>{
     try {
         const {firstName, lastName, program, cohort} = req.body
         let studentToCreate = req.body
-        if(!studentToCreate){
-            return res.status(404).json({message: "Fill all fields to create a human"})
+        if(!firstName || !lastName || !program || !cohort){
+            return res.status(404).json({message: "Fill all fields to create a student"})
         }
         
-        const createdStudent = await Student.create({studentToCreate})
+        const createdStudent = await Student.create({ firstName, lastName, program, cohort })
         res.status(201).json(createdStudent)
 
 	} catch (error) {
 		console.log(error)
+        res.status(500).json({ message: "Internal server error" });
 	}
 })
 
 // route to update a specified student by Id
-router.put("/students/:studentsId", async(req, res, next)=>{
+router.put("/students/:studentId", async(req, res, next)=>{
     try {
         const {firstName, lastName, program, cohort} = req.body 
-        const id = req.params.studentsId
+        const id = req.params.studentId
         const studentToUpdate = {firstName, lastName, program, cohort}
         const newStudent = await Student.findByIdAndUpdate(id, studentToUpdate, {new: true})
         res.json(newStudent)
     } catch (error) {
         console.log(error)
+        res.status(500).json({ message: "Internal server error" });
         
     }
 })
 
 // route to delete a specific student by Id
-router.delete("/students/:studentsId", async(req, res, next)=>{
-    const id = req.params.studentsId
+router.delete("/students/:studentId", async(req, res, next)=>{
+    const id = req.params.studentId
     try {
         await Student.findByIdAndDelete(id)
         res.json({message : `Student ${id} was deleted`})
 
     } catch (error) {
         console.log(error)
+        res.status(500).json({ message: "Internal server error" });
     }
 
 })
